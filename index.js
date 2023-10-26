825afbcce09b41206b3d05e89dfb27a2ac8ad8e3
litonahun <139342589+litonahun@users.noreply.github.com>
2023-10-26 14:42:01 +0300

    Intial commit of to-do-list app

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+let newItems = [];
+app.post("/submit", (req, res) => {
+  if (req.body.newItem) {
+    newItems.push(req.body.newItem);
+  }
+
+  res.render("index.ejs", {
+    Tasks: newItems,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
